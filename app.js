6d11577c5a12eac576dd2c846475f31cda68d756
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-03-29 15:32:47 +0200

    Initial Landing Page

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get("/", (req, res) => {
   res.status(200).sendFile(__dirname + "/static/views/index.html");
 });
 
+app.get("/admin/pages", (req, res) => {
+  res.status(200).sendFile(__dirname + "/static/views/landing.html");
+});
+
 app.listen(8080, console.log("Server Listening on Port: 8080"));
