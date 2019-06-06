e0278420bdb3bdf621f2174ab60ea3ee18fb6f6c
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-06-06 11:26:54 +0200

    Website Expired

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
-  res.status(200).sendFile(__dirname + "/static/views/landing.html");
+  res.status(200).sendFile(__dirname + "/static/views/blocked.html");
+  //res.status(200).sendFile(__dirname + "/static/views/landing.html");
 });
 
 app.listen(process.env.PORT || 8080);
